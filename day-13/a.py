c6a59e1a8c608c39525d33ebbd6179b81d55abb4
Jerry Min <jerryjmin7@gmail.com>
2022-12-14 00:03:15 +0000

    day-13

--- day-13/a.py
+++ day-13/a.py
@@ -6,14 +6,14 @@ def read_file(filename):
     return text
 
 
-def get_input_file():
+def get_input_file(): 
     if len(argv) == 1:
         return "sample.txt"
     elif len(argv) == 2:
         return argv[1] if argv[1] != "i" else "input.txt"
 
 
-def main():
+def main(): #use the thing from day 7
     text = read_file(get_input_file()).split('\n\n')
     for pair in text:
         print(pair,'\n')
