8f57b765908fc6d339eed6a4aed397f6a997ca0d
Jerry Min <116847418+xccn-xccn@users.noreply.github.com>
2022-12-14 17:03:58 +0000

    day-13

--- day-13/a.py
+++ day-13/a.py
@@ -13,10 +13,21 @@ def get_input_file():
         return argv[1] if argv[1] != "i" else "input.txt"
 
 
+def right(pair):
+    first,second = pair
+    if isinstance(first,int) and isinstance(second,int):
+        if first == second:
+            return right(pair[1:])
+    elif isinstance(first,list) and isinstance(second,list):
+        return right((first,second))
+    
 def main(): #use the thing from day 7
     text = read_file(get_input_file()).split('\n\n')
+    correct_count = 0
     for pair in text:
         print(pair,'\n')
+        if right(pair):
+            correct_count += 1
 
 if __name__ == "__main__":
     print(main())
