6e0a893b560a57fabfa4d7ff0de742f65c600664
Jerry Min <jerryjmin7@gmail.com>
2022-12-21 13:26:28 +0000

    random

--- test.py
+++ test.py
@@ -1,8 +1,2 @@
-square = [[3, 0, 3, 7, 3],
-[2, 5, 5, 1, 2],
-[6, 5, 3, 3, 2],
-[3, 3, 5, 4, 9],
-[3, 5, 3, 9, 0]]
-for y_index,line in enumerate(square[1:-1], 1):
-    for x_index,tree in enumerate(line[1:-1], 1):
-        print(x_index,y_index,tree)
+for i, value in enumerate([1,2,3,4,5],start= 1):
+    print(i,value)
